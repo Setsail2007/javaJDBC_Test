77861864057e604b42d07c90b532fe60adc87c41
Setsail2007 <2817946077@qq.com>
2026-06-30 21:51:45 +0800

    Delete user.sql

--- user.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (1, '张三', 123456);
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (2, '李四', 654321);
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (3, '王五', 12345);
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (4, '小明', 1234);
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (5, '小王', 123);
-INSERT INTO `user` (`id`, `user`, `password`) VALUES (7, '啊大大', 20135);
